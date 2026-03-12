abab7236745469f44455bacee82e9f6eb5695896
Dipesh Chaudhary <101973614+dipeshjatt2@users.noreply.github.com>
2026-03-12 12:02:47 +0530

    Create profiledef.sh for ISO build settings
    Add profiledef.sh for BhagwaOS ISO configuration

---
+++ profile/profiledef.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="bhagwaos"
+iso_label="BHAGWA_$(date +%Y%m)"
+iso_publisher="BhagwaOS"
+iso_application="BhagwaOS Live/Installation Media"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x64.grub.esp' 'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+)
+
