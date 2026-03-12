dfa17b0bc106234a3b362552cb4c426361e24f77
Dipesh Chaudhary <101973614+dipeshjatt2@users.noreply.github.com>
2026-03-12 12:09:34 +0530

    Remove permissions for polkit-1 rules
    Remove file permissions definition for polkit rules.

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -7,15 +7,15 @@ iso_publisher="BhagwaOS"
 iso_application="BhagwaOS Live/Installation Media"
 install_dir="arch"
 buildmodes=('iso')
-# Use modern bootmodes
 bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+
+# We only define permissions for files/folders we are manually providing
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
-  ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
 )
